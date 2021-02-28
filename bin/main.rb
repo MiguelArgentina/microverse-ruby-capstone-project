f18f958c11ce7988e6cca1d26ec903c0371dc9ae
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-28 02:37:39 -0300

    Adjust method call to meet new implementation

--- bin/main.rb
+++ bin/main.rb
@@ -2,10 +2,6 @@
 require_relative '../lib/weather_bot'
 require_relative '../lib/bot_state'
 
-
-
-  bot = WeatherBot.new
-
   current_state = BotState::LISTENING
   list_sent = false
-  bot.start_bot(current_state, list_sent)
+  bot = WeatherBot.new(current_state, list_sent)
