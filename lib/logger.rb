f58c661f6bf7cb10a2360060517bc768da314eba
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-28 02:37:06 -0300

    Add error handling to the Alogger class

--- lib/logger.rb
+++ lib/logger.rb
@@ -11,13 +11,20 @@ class Alogger
     method(private_method.to_sym).call(params)
   end
 
-
   private
   def logger_create_log(*params)
-    file = File.open('./assets/log/weatherbot.log', File::WRONLY | File::APPEND)
-    logfile = Logger.new(file)
-    logfile.info(params[0]) { params[1] }
-    return true if logfile.close
-    false
+    what_to_return = true
+    begin
+      file = File.open('./assets/log/weatherbot.log', File::WRONLY | File::APPEND)
+      logfile = Logger.new(file)
+      logfile.info(params[0]) { params[1] }
+
+    rescue
+      what_to_return = false
+    ensure
+      logfile.close unless logfile.nil?
+      what_to_return
+    end
+
   end
 end
