566385fd3c873fd02434239a95d6a0c8b68100b1
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-27 21:56:46 -0300

    Updated main.rb code because of changes in weather_bot.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 require_relative '../lib/weather_bot'
+require_relative '../lib/bot_state'
 
-WeatherBot.new
+
+
+  bot = WeatherBot.new
+
+  current_state = BotState::LISTENING
+  list_sent = false
+  bot.start_bot(current_state, list_sent)
