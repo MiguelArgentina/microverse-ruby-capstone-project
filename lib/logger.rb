82bbe7833cb2f9a2365695e2c76a49c83de4f317
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-27 21:48:04 -0300

    Change logger.rb from module to class and add a handler to access the private method

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,17 +1,23 @@
 require 'logger'
 
 # :nodoc:
-module Alogger
-  # def initialize(_aux = nil)
-  #
-  # end
+class Alogger
 
-  def logger_create_log(log_title, message)
+  def initialize()
+
+  end
+
+  def log_creator_handler(private_method, *params)
+    method(private_method.to_sym).call(params)
+  end
+
+
+  private
+  def logger_create_log(*params)
     file = File.open('./assets/log/weatherbot.log', File::WRONLY | File::APPEND)
     logfile = Logger.new(file)
-    logfile.info(log_title) { message }
+    logfile.info(params[0]) { params[1] }
     return true if logfile.close
-
     false
   end
 end
